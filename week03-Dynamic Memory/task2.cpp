7f2ba876f0196a241f9d6c946d2d390a0339808f
DeyvidAdzhemiev <60064484+DeyvidAdzhemiev@users.noreply.github.com>
2020-03-24 14:03:19 +0200

    Update name

---
+++ week03-Dynamic Memory/task2.cpp
@@ -0,0 +1,34 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//// Напишете имплементация на strcpy(char* dest, const char* src) функцията.
+//
+//int mystrlen(const char* str)
+//{
+//	int i = -1;
+//	while (str[++i]);
+//	return i;
+//}
+//void mystrcpy(char*& destination, const char* source)
+//{
+//	delete[] destination;
+//
+//	int length = mystrlen(source);
+//
+//	destination = new char[length + 1]; // '\0'
+//	for (int i = 0; i < length; i++)
+//	{
+//		destination[i] = source[i];
+//	}
+//}
+//
+//
+//int main()
+//{
+//	const char* src = "Deyvid";
+//	char* dest = nullptr;
+//	mystrcpy(dest, src);
+//
+//	cout << "Source: " << src << endl;
+//	cout << "Dest: " << dest << endl;
+//}
---
+++ week03-Dynamic Memory/task6.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+using namespace std;
+
+//Да се направи клас Телефон, който в себе си има марка, модел, цена.За този клас трябва да се реализират :
+
+//--по подразбиране
+//--конструктор с параметри
+//--деструктор
+//--оператор за присвояване
+
+class Telephone
+{
+private:
+	char* brand;
+	char* model;
+	double price;
+
+	void copy(const Telephone& other)
+	{
+		this->brand = new char[strlen(other.brand) + 1];
+		this->model = new char[strlen(other.model) + 1];
+		for (int i = 0; i < strlen(other.brand) + 1; i++)
+		{
+			this->brand[i] = other.brand[i];
+		}
+		for (int i = 0; i < strlen(other.model) + 1; i++)
+		{
+			this->model[i] = other.model[i];
+		}
+		this->price = other.price;
+	}
+
+	void erase()
+	{
+		delete[] this->brand;
+		delete[] this->model;
+	}
+public:
+	Telephone()
+	{
+		this->brand = nullptr;
+		this->model = nullptr;
+		this->price = 0;
+	}
+	
+	Telephone(const char* brand, const char* model, const double price)
+	{
+		this->brand = new char[strlen(brand) + 1];
+		this->model = new char[strlen(model) + 1];
+		for (int i = 0; i < strlen(brand) + 1; i++)
+		{
+			this->brand[i] = brand[i];
+		}
+		for (int i = 0; i < strlen(model) + 1; i++)
+		{
+			this->model[i] = model[i];
+		}
+		this->price = price;
+	}
+
+	Telephone(const Telephone& other)
+	{
+		this->copy(other);
+	}
+
+	Telephone& operator= (const Telephone& other)
+	{
+		if (this != &other)
+		{
+			this->erase();
+			this->copy(other);
+		}
+		return *this;
+	}
+
+	~Telephone()
+	{
+		this->erase();
+	}
+
+	void print() const
+	{
+		cout << "Brand: " << this->brand << endl;
+		cout << "Model: " << this->model << endl;
+		cout << "Price: " << this->price << endl;
+	}
+};
+
+
+int main()
+{
+	Telephone telephone("iPhone", "11", 1699.99);
+
+		telephone.print();
+}
